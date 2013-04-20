34ebe10a88b072ab868daf81dbb3619436325a73
Isantipov <i.antipov@itransition.com>
2013-04-20 14:28:35 +0300

    Add ConnectHub

---
+++ src/HotScroll.Server/HotScroll.Server/Hubs/ConnectHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace HotScroll.Server.Hubs
+{
+    /// <summary>
+    ///     Manage connections.
+    /// </summary>
+    public class ConnectHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
