30c56147e94f27646cb9131529335a136fd08574
Alex Skakun <alex.skakun@me.com>
2013-04-20 16:06:54 +0300

    searching message

--- src/HotScroll.Client/HotScroll.Client/HotScroll.Client/js/game.js
+++ src/HotScroll.Client/HotScroll.Client/HotScroll.Client/js/game.js
@@ -5,20 +5,25 @@
         ready: function () {
             
             window.onload = function () {
-                var countdown = document.getElementById('countdown');
-                countdown.innerHTML = '3';
-                setTimeout(function () {
-                    countdown.innerHTML = '2';
+                var searching = document.querySelector('.searching');
 
+                function startCountDown() {
+                    var countdown = document.getElementById('countdown');
+                    countdown.innerHTML = '3';
                     setTimeout(function () {
-                        countdown.innerHTML = '1';
+                        countdown.innerHTML = '2';
 
                         setTimeout(function () {
-                            countdown.style.display = 'none';
-                            document.getElementById('game').style.display = 'block';
+                            countdown.innerHTML = '1';
+
+                            setTimeout(function () {
+                                countdown.style.display = 'none';
+                                document.getElementById('game').style.display = 'block';
+                            }, 1000);
                         }, 1000);
                     }, 1000);
-                }, 1000);
+                }
+
             };
         }
     });
