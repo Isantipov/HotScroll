615e964acfeee513c940838ac3bf3716960ba91f
Dmitry <dmitry.parfenchik@gmail.com>
2013-04-20 16:50:57 +0300

    Domain for game + servicies

---
+++ src/HotScroll.Server/HotScroll.Server/Domain/Duel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotScroll.Server.Domain
+{
+    public class Duel
+    {
+        public string Id { get; set; }
+        public User Player1 { get; set; }
+        public User Player2 { get; set; }
+
+        public List<Step> Steps { get; set; }
+
+        public Duel()
+        {
+            Id = Guid.NewGuid().ToString();
+            Steps = new List<Step>();
+        }
+    }
+}
