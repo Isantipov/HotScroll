bfda9314382c754c6ec3e28db699efe9ac3b2bd3
AndreyPetrov1991 <19graff91@gmail.com>
2013-04-20 14:50:56 +0300

    game page

---
+++ src/HotScroll.Client/HotScroll.Client/HotScroll.Client/js/game.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+
+    WinJS.UI.Pages.define('/game.html', {
+        ready: function () {
+            
+            window.onload = function () {
+                var countdown = document.getElementById('countdown');
+                countdown.innerHTML = '3';
+                setTimeout(function () {
+                    countdown.innerHTML = '2';
+
+                    setTimeout(function () {
+                        countdown.innerHTML = '1';
+
+                        setTimeout(function () {
+                            countdown.innerHTML = 'Go!';
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            };
+        }
+    });
+});
