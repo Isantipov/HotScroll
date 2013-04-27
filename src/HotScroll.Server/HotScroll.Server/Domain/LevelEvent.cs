965cb34900d66ae914d7fdc4ca16a3dba7650ca4
2ooom <dmitry.parfenchik@gmail.com>
2013-04-27 17:47:56 +0300

    Duel extended with levele information. Level property added with Background and LevelEvents collections providing GenearateRandom to fill the object in for the first time

---
+++ src/HotScroll.Server/HotScroll.Server/Domain/LevelEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HotScroll.Server.Domain
+{
+    public class LevelEvent
+    {
+        #region Constants
+
+        protected const int MaximumDuration = 300;
+        protected const int MinimumDuration = 50;
+
+        #endregion
+
+        public int Score { get; set; }
+        public EventType Type { get; set; }
+        public int Duration { get; set; }
+
+        public LevelEvent()
+        {
+            Type = EventType.LeftDistractor;
+        }
+
+        public void GenerateRandom()
+        {
+            var random = new Random();
+            Duration = random.Next(MinimumDuration, MaximumDuration);
+            var maxEvent = Enum.GetNames(typeof (EventType)).Length;
+            Type = (EventType) random.Next(1, maxEvent);
+        }
+    }
+
+    public enum EventType
+    {
+        LeftDistractor  = 1,
+
+        //RightDistractor = 2,
+        //MegaDistractor = 3,
+    }
+}
---
+++ src/HotScroll.Server/HotScroll.Server/Domain/LevelMap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotScroll.Server.Domain
+{
+    public class LevelMap
+    {
+        #region Constants
+
+        // Tiles
+        protected const int UniqueTilesNumber = 5;
+        protected const int TilesNumber = 15;
+
+        // Events
+        protected const int EventsNumber = 10;
+
+        #endregion
+
+        public List<int> Background { get; set; }
+
+        public List<LevelEvent> Events { get; set; }
+
+        public LevelMap()
+        {
+            Background = new List<int>();
+            Events = new List<LevelEvent>();
+        }
+
+        protected void GenerateRandom()
+        {
+            // Background
+            Background.Clear();
+            var random = new Random();
+            for(var i = 0; i < TilesNumber; i++)
+            {
+                Background.Add(random.Next(1, UniqueTilesNumber));
+            }
+
+            // Events
+            Events.Clear();
+            for(var i = 0; i < EventsNumber; i++)
+            {
+                var evnt = new LevelEvent();
+                evnt.GenerateRandom();
+                Events.Add(evnt);
+            }
+        }
+    }
+}
