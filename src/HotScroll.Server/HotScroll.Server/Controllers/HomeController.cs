412d564d3b57094a2c68eb4be39275e7f0b0b770
2ooom <dmitry.parfenchik@gmail.com>
2013-04-27 19:35:32 +0300

    Unused files clean up. AccountController removed and Test renamed to Index

--- src/HotScroll.Server/HotScroll.Server/Controllers/HomeController.cs
+++ src/HotScroll.Server/HotScroll.Server/Controllers/HomeController.cs
@@ -6,29 +6,6 @@ namespace HotScroll.Server.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        // GET /Test
-        
-        public ActionResult Test()
-        {
             return View();
         }
     }
