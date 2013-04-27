3e8ccfc237c00cd2eeed67ba798358d51196a7ab
Yauheni Leichanok <e.leychenok@gmail.com>
2013-04-27 09:52:17 -0700

    Added two login steps using navigational system.

---
+++ src/HotScroll.Client/HotScroll.Client/HotScroll.Client/pages/lobby/lobby.js
@@ -0,0 +1,14 @@
+﻿(function () {
+
+    'use strict';
+
+    var storage = Windows.Storage.ApplicationData.current.localSettings;
+
+    WinJS.UI.Pages.define('/pages/lobby/lobby.html', {
+        ready: function () {
+            $('#lobby > img').attr('src', storage.values.currentUser.picture);
+            $('#lobby > h2').text('Welcome, ' + storage.values.currentUser.name + '!');
+        }
+    });
+
+})();
