f7c45372d59efc490e6906c67d46c9951a46348f
Yauheni Leichanok <e.leychenok@gmail.com>
2013-05-02 09:05:57 -0700

    Added countdown and map generation.

---
+++ src/HotScroll.Client/HotScroll.Client/HotScroll.Client/pages/game/game.js
@@ -0,0 +1,48 @@
+﻿(function () {
+
+    'use strict';
+
+    var storage = Windows.Storage.ApplicationData.current.localSettings;
+
+    WinJS.UI.Pages.define('/pages/game/game.html', {
+        ready: function () {
+            this._prepareLevel(window.duel.Level);
+            this._startCountdown(3);
+        },
+
+        _prepareLevel: function (levelData) {
+            var track1 = $('#track1'),
+                track2 = $('#track2');
+
+            var html = [];
+            for (var i = 0; i < levelData.Background.length; i++) {
+                html.push('<div class="track-part track-part_' + levelData.Background[i] + '"></div>');
+            }
+
+            track1.html(html.join(''));
+            track2.html(html.join(''));
+        },
+
+        _startCountdown: function (seconds) {
+            var count = seconds,
+                that = this;
+
+            $('#countdown').text(count);
+
+            var interval = setInterval(function () {
+                count--;
+                if (count === 0) {
+                    $('#countdown').fadeOut('fast');
+                    that._startGame();
+                } else {
+                    $('#countdown').text(count);
+                }
+            }, 1000);
+        },
+
+        _startGame: function () {
+
+        }
+    });
+
+})();
