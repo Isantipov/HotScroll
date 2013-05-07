b698b838026e364bfa5dc87a0a0fc8a9c9784d87
Ivan Antsipau <i.antipov@itransition.com>
2013-05-07 21:19:56 +0300

    reformat code

--- src/HotScroll.Server/HotScroll.Server/Hubs/GameHub.cs
+++ src/HotScroll.Server/HotScroll.Server/Hubs/GameHub.cs
@@ -10,14 +10,15 @@ namespace HotScroll.Server.Hubs
     /// </summary>
     public class GameHub : Hub
     {
+        private readonly Game game;
 
-        private readonly Game _game;
-
-        public GameHub() : this(Game.Instance) { }
+        public GameHub() : this(Game.Instance)
+        {
+        }
 
         public GameHub(Game game)
         {
-            _game = game;
+            this.game = game;
         }
 
         public Duel CreateDuel(Player player)
@@ -33,44 +34,45 @@ namespace HotScroll.Server.Hubs
         public Player Connect(Player player)
         {
             player.ConnectionId = Context.ConnectionId;
-            _game.PlayerService.Add(player);
+            game.PlayerService.Add(player);
             Clients.All.playerConnected(player);
+
             return player;
         }
 
         public DuelProjection WaitPartner(Player player)
         {
-            var serverPlayer = _game.PlayerService.Get(player.ConnectionId);
+            Player serverPlayer = game.PlayerService.Get(player.ConnectionId);
             serverPlayer.Status = PlayerStatus.WaitingForPartner;
-            var oponent = _game.PlayerService.GetFreePlayer(serverPlayer);
-            
+            Player oponent = game.PlayerService.GetFreePlayer(serverPlayer);
+
             if (oponent != null)
             {
                 serverPlayer.Status = oponent.Status = PlayerStatus.Playing;
                 var duel = new Duel(new List<Player> {serverPlayer, oponent});
-                _game.DuelService.Add(duel);
+                game.DuelService.Add(duel);
 
-                var proj1 = duel.ToProjection(serverPlayer.ConnectionId);
+                DuelProjection proj1 = duel.ToProjection(serverPlayer.ConnectionId);
                 Clients.Client(serverPlayer.ConnectionId).play(proj1);
 
-                var proj2 = duel.ToProjection(oponent.ConnectionId);
+                DuelProjection proj2 = duel.ToProjection(oponent.ConnectionId);
                 Clients.Client(oponent.ConnectionId).play(proj2);
                 return proj1;
             }
 
             return null;
         }
-        
+
         public void RecordStep(Step step)
         {
-            var player = _game.PlayerService.Get(step.PlayerId);
-            var duel = _game.DuelService.GetDuelForPLayer(player.ConnectionId);
+            Player player = game.PlayerService.Get(step.PlayerId);
+            Duel duel = game.DuelService.GetDuelForPLayer(player.ConnectionId);
             if (duel == null || duel.IsGameOver)
             {
                 return;
             }
 
-            var opponent = duel.GetOpponent(player.ConnectionId);
+            Player opponent = duel.GetOpponent(player.ConnectionId);
             Clients.Client(opponent.ConnectionId).receiveStep(step);
         }
 
@@ -80,8 +82,8 @@ namespace HotScroll.Server.Hubs
         /// </summary>
         public void RecordFinished()
         {
-            var player = _game.PlayerService.Get(Context.ConnectionId);
-            var duel = _game.DuelService.GetDuelForPLayer(player.ConnectionId);
+            Player player = game.PlayerService.Get(Context.ConnectionId);
+            Duel duel = game.DuelService.GetDuelForPLayer(player.ConnectionId);
             if (duel == null)
             {
                 return;
@@ -94,8 +96,8 @@ namespace HotScroll.Server.Hubs
                     return;
                 }
 
-                _game.DuelService.FinishAndRemove(duel);
-                var opponent = duel.GetOpponent(player.ConnectionId);
+                game.DuelService.FinishAndRemove(duel);
+                Player opponent = duel.GetOpponent(player.ConnectionId);
 
                 Clients.Client(opponent.ConnectionId).gameOver(hasWon: false);
                 Clients.Caller.gameOver(hasWon: true);
