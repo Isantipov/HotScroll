2fc2375e016ca55f141403f93ea829ec8bc66f1c
2ooom <dmitry.parfenchik@gmail.com>
2013-05-07 20:50:32 +0300

    Corrections to game private fileds

--- src/HotScroll.Server/HotScroll.Server/Game.cs
+++ src/HotScroll.Server/HotScroll.Server/Game.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using HotScroll.Server.Services;
 
 namespace HotScroll.Server
 {
     public class Game
     {
-        private readonly static Lazy<Game> _instance = new Lazy<Game>(() => new Game());
+        private readonly static Lazy<Game> InstanceInternal = new Lazy<Game>(() => new Game());
 
-        public static Game Instance { get { return _instance.Value; } }
+        public static Game Instance { get { return InstanceInternal.Value; } }
 
         public PlayerService PlayerService { get; private set; }
         public DuelService DuelService { get; private set; }
