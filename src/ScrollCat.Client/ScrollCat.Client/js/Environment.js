c01f6941aa5ea0bacf3a7fd2315fe6bd1c2b8096
2ooom <dmitry.parfenchik@gmail.com>
2013-05-10 01:38:55 +0300

    Background generation correction. Upper bound increased to n + 1

--- src/ScrollCat.Client/ScrollCat.Client/js/Environment.js
+++ src/ScrollCat.Client/ScrollCat.Client/js/Environment.js
@@ -2,10 +2,13 @@ var Environment = {
     initialize: function () {
         this.mountains = document.querySelector('#mountains');
         this.ground = document.querySelector('#ground');
+        this.bgObjects = document.querySelector('#bg-objects');
+        //var bgObjs = game.duel.
     },
 
     move: function (scorePercent) {
         this.mountains.style.backgroundPosition = -scorePercent * 7 + '% 0';
         this.ground.style.backgroundPosition = -scorePercent * 14 + '% 0';
+        this.bgObjects.style.backgroundPosition = -scorePercent * 11 + '% 0';
     }
 };
