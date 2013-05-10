f57551a23424c46137cbd27903d644cca7f093a0
2ooom <dmitry.parfenchik@gmail.com>
2013-05-10 03:40:13 +0300

    BgItemTypes added to BgItem

---
+++ src/HotScroll.Server/HotScroll.Server/Domain/BgItemType.cs
@@ -0,0 +1,13 @@
+﻿namespace HotScroll.Server.Domain
+{
+    public class BgItemType
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool IsOverlapping { get; set; }
+        public bool IsReplicable { get; set; }
+        public int? Margin { get; set; }
+        public int WidthPx { get; set; }
+        public int Size { get { return WidthPx/14; } }
+    }
+}
