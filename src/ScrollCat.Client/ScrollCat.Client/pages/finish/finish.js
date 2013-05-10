2b2e3128f11be4d48cb4834456448adae77aa23e
2ooom <dmitry.parfenchik@gmail.com>
2013-05-10 19:57:17 +0300

    Sound efeects added to win-loose screen

--- src/ScrollCat.Client/ScrollCat.Client/pages/finish/finish.js
+++ src/ScrollCat.Client/ScrollCat.Client/pages/finish/finish.js
@@ -7,9 +7,14 @@
             if (options.hasWon === true) {
                 $('#finish-container').addClass('win');
                 $('h1').text('You win!');
+                $('#winEffect').play();
+                setTimeout(function() {
+                    $('#winEffect').play();
+                }, 2500);
             } else {
                 $('#finish-container').addClass('lose');
                 $('h1').text('You lose!');
+                $('#looseEffect').play();
             }
 
             $('#menu').click(this._menu);
