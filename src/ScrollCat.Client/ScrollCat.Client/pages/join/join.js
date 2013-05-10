db1efcecac41366cdfbe19818e96061bf8c5435a
2ooom <dmitry.parfenchik@gmail.com>
2013-05-10 16:26:14 +0300

    Join page added to client

---
+++ src/ScrollCat.Client/ScrollCat.Client/pages/join/join.js
@@ -0,0 +1,56 @@
+﻿(function () {
+    'use strict';
+
+    WinJS.UI.Pages.define('/pages/join/join.html', {
+        ready: function () {
+            document.querySelector('#mainTheme').play();
+
+            $('#login').val(game.player.Name);
+                
+            $('#loadingIndicator').addClass('hidden');
+            setTimeout(function () {
+                $('#loadingIndicator').hide();
+                $('#credentials').show().addClass('visible');
+            }, 300);
+
+            var that = this;
+            $('#play').click(function () {
+                that._play();
+            });
+        },
+        _loginAndWait: function(afterLoginCallBack) {
+            var login = $('#login').val();
+            if (login !== '') {
+                this._showHelp(function () {
+
+                    $('.login-container').hide();
+                    $('.wait-container').show();
+                    
+                    game.setPlayerName(login);
+                    afterLoginCallBack(login);
+
+                });
+            } else {
+                $('#validation-message').show();
+            }
+        },
+        _play: function () {
+            this._loginAndWait(game.loginAndJoinDuel);
+        },
+        
+        _showHelp: function (callback) {
+            if (!game.getHelpShown()) {
+                $('#help').fadeIn();
+                $('#help-close').click(function () {
+                    game.setHelpShown(true);
+                    $('#help').fadeOut();
+                    callback();
+                });
+            } else {
+                callback();
+            }
+        }
+
+    });
+
+})();
