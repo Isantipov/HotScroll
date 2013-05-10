816d4f8206ae91beb15dc2cbdaeb1bc3a945d551
2ooom <dmitry.parfenchik@gmail.com>
2013-05-10 03:40:13 +0300

    BgItemTypes added to BgItem

--- src/HotScroll.Server/HotScroll.Server/Domain/BgItem.cs
+++ src/HotScroll.Server/HotScroll.Server/Domain/BgItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace HotScroll.Server.Domain
@@ -8,18 +9,28 @@ namespace HotScroll.Server.Domain
         #region Constants
 
         protected const int MaximumOffset = 1000;
-        protected const int MaximumType = 7;
 
         #endregion
 
-        public int Type { get; set; }
+        public BgItemType Type { get; set; }
+        public string Code { get; set; }
         public int Offset { get; set; }
 
+        public static List<BgItemType> BgItemTypes = new List<BgItemType>
+            {
+                new BgItemType{Code = "small_tree", Name = "Small Tree", IsOverlapping = true, IsReplicable = true, WidthPx = 235},
+                new BgItemType{Code = "big_tree", Name = "Big Tree", IsOverlapping = true, IsReplicable = true, WidthPx = 337},
+                new BgItemType{Code = "bush", Name = "Bush", IsOverlapping = true, IsReplicable = true, WidthPx = 195},
+                new BgItemType{Code = "bench", Name = "Bench", IsOverlapping = false, IsReplicable = false, WidthPx = 341},
+                new BgItemType{Code = "fireplug", Name = "Fireplug", IsOverlapping = false, IsReplicable = false, WidthPx = 89},
+                new BgItemType{Code = "purple_flower", Name = "Purple Flower", IsOverlapping = false, IsReplicable = true, WidthPx = 53},
+                new BgItemType{Code = "yellow_flower", Name = "Yellow Flower", IsOverlapping = false, IsReplicable = true, WidthPx = 53},
+            };
 
         public void GenerateRandom(Random random)
         {
             Offset = random.Next(0, MaximumOffset);
-            Type = random.Next(1, MaximumType + 1);
+            Type = BgItemTypes[random.Next(0, BgItemTypes.Count)];
         }
     }
 }
