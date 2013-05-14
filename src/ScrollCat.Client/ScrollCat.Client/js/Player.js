871ce3654a812ab51868e36c8b4c4baf68b2368a
2ooom <dmitry.parfenchik@gmail.com>
2013-05-14 03:41:37 +0300

    Players animation concurrect corrections

--- src/ScrollCat.Client/ScrollCat.Client/js/Player.js
+++ src/ScrollCat.Client/ScrollCat.Client/js/Player.js
@@ -1,4 +1,6 @@
-﻿function Player (name, isOpponent, template) {
+﻿function Player(name, isOpponent, template) {
+
+    var _this = this;
 
     this.name = name;
     this.isOpponent = isOpponent;
@@ -13,75 +15,75 @@
     this.inertMovement = 0;
     this.butterfly = null;
     this.timestamp = 0;
+
+    this.setScore = function(score) {
+        _this.score = score;
+        var scorePercent = ((_this.score / game.TOTAL_SCORE) * 100).toFixed(5);
+        _this.icon.style.left = scorePercent + '%';
+        if (!_this.isOpponent) {
+            game.recordStep(_this.score);
+            Environment.move(scorePercent);
+            game.opponentPlayer.setScore(game.opponentPlayer.score);
+        } else {
+            var bgPercent = parseFloat(Environment.ground.style.backgroundPosition);
+            var opponentBgPercent = (game.opponentPlayer.score / game.TOTAL_SCORE) * 100 * 14;
+            // 428 - ground pattern image width in px
+            _this.element.style.left = window.innerWidth / 2 + (window.innerWidth * (bgPercent / 100) - (bgPercent / 100) * 428) + (window.innerWidth * (opponentBgPercent / 100) - (opponentBgPercent / 100) * 428) + 'px';
+        }
+    };
+
+    this.animateCat = function(timeout) {
+        var style = _this.element.style;
+        var bgPos = parseInt(style.backgroundPosition);
+        bgPos -= 350 * _this.direction;
+
+        if (Math.abs(bgPos) > 7700) {
+            style.backgroundPositionX = '0px';
+        } else if (bgPos > 0) {
+            style.backgroundPositionX = '-7700px';
+        } else {
+            style.backgroundPositionX = bgPos + 'px';
+        }
+
+        if (timeout < 120) {
+            _this.newTimeOut = 1.5 * timeout;
+            _this.animationTimer = setTimeout(_this.animateCatByTimeout, _this.newTimeOut);
+        }
+    };
+
+    this.animateCatByTimeout = function() {
+        _this.animateCat(_this.newTimeout);
+        var newScore = _this.score + _this.direction;
+        _this.setScore(newScore);
+    };
+
+    this.playAnimation = function(event, direction) {
+        _this.direction = direction;
+        _this.animateCat(event.timeStamp - _this.timestamp);
+        _this.timestamp = event.timeStamp;
+    };
+
+    this.rotate = function() {
+        if (_this.element.className.indexOf('rotated') < 0) {
+            _this.element.className += ' rotated';
+        } else {
+            _this.element.className = _this.element.className.replace('rotated', '');
+        }
+    };
+    this.rotateRightDirection = function() {
+        if (_this.rightDirection === -1) {
+            _this.rightDirection = 1;
+        } else {
+            _this.rightDirection = -1;
+        }
+    };
+
+    this.initializeCat = function() {
+        $(_this.element).addClass(_this.templateClass);
+        $(_this.icon).addClass(_this.iconClass);
+    };
+    
+    this.stopAnimation = function() {
+        clearTimeout(_this.animationTimer);
+    };
 }
-
-Player.prototype.setScore = function (score) {
-    this.score = score;
-    var scorePercent = ((this.score / game.TOTAL_SCORE) * 100).toFixed(5);
-    this.icon.style.left = scorePercent + '%';
-    if (!this.isOpponent) {
-        game.recordStep(this.score);
-        Environment.move(scorePercent);
-        game.opponentPlayer.setScore(game.opponentPlayer.score);
-    } else {
-        var bgPercent = parseFloat(Environment.ground.style.backgroundPosition);
-        var opponentBgPercent = (game.opponentPlayer.score / game.TOTAL_SCORE) * 100 * 14;
-        // 428 - ground pattern image width in px
-        this.element.style.left = window.innerWidth / 2 + (window.innerWidth * (bgPercent / 100) - (bgPercent / 100) * 428) + (window.innerWidth * (opponentBgPercent / 100) - (opponentBgPercent / 100) * 428) + 'px';
-    }
-};
-
-Player.prototype.animateCat = function (timeout, direction) {
-    var that = this,
-        style = this.element.style;
-
-    var bgPos = parseInt(style.backgroundPosition, 10);
-    bgPos -= 350 * direction;
-
-    if (Math.abs(bgPos) > 7700) {
-        style.backgroundPositionX = '0px';
-    } else if (bgPos > 0) {
-        style.backgroundPositionX = '-7700px';
-    } else {
-        style.backgroundPositionX = bgPos + 'px';
-    }
-
-    if (timeout < 120) {
-        var newTimeout = 1.5 * timeout;
-        that.interval = setTimeout(function () {
-            that.animateCat(newTimeout, direction);
-            
-            var newScore = game.currentPlayer.score + direction;
-            if (newScore <= game.TOTAL_SCORE && newScore >= 0) {
-                //Butterfly.matchScore(game.currentPlayer.score, game.currentPlayer);
-                game.currentPlayer.setScore(newScore);
-            }
-        }, newTimeout);
-    }
-};
-
-Player.prototype.playAnimation = function (event, direction) {
-    clearTimeout(this.interval);
-    this.animateCat(event.timeStamp - this.timestamp, direction);
-    this.timestamp = event.timeStamp;
-};
-
-Player.prototype.rotate = function () {
-    if (this.element.className.indexOf('rotated') < 0) {
-        this.element.className += ' rotated';
-    } else {
-        this.element.className = this.element.className.replace('rotated', '');
-    }
-};
-Player.prototype.rotateRightDirection = function () {
-    if (this.rightDirection === -1) {
-        this.rightDirection = 1;
-    } else {
-        this.rightDirection = -1;
-    }
-};
-
-Player.prototype.initializeCat = function () {
-    $(this.element).addClass(this.templateClass);
-    $(this.icon).addClass(this.iconClass);
-};
